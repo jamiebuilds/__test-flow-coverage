5d4e6be4c5dcec0c03a68171675ad2d90b6fd241
James Kyle <me@thejameskyle.com>
2017-07-19 15:36:36 +1000

    init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// @flow
+
+import React from 'react';
+
+function wrap<Props: {}>(Component: Class<React.Component<void, Props, void>>): Class<React.Component<void, Props, void>> {
+  return class MyComponent extends React.Component {
+    props: Props;
+
+    render() {
+      return <Component {...this.props}/>;
+    }
+  }
+}
+
+class Hello extends React.Component {
+  props: {
+    target: string,
+  };
+
+  render() {
+    return <h1>Hello {this.props.target}!</h1>;
+  }
+}
+
+let WrappedHello = wrap(Hello);
+let test = <WrappedHello target={42}/>;
